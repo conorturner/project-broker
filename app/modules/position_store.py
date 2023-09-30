28e65452d09e132ff5f80c6a8e57513b2289aa72
conorturner <conorscturner@gmail.com>
2023-09-30 10:29:47 +0100

    lint position_store.py

--- app/modules/position_store.py
+++ app/modules/position_store.py
@@ -1,2 +1,20 @@
+"""
+Contains classes and methods for storing position data.
+"""
+
+
 class PositionStore:
-    pass  # TODO: store position and keep live returns updated via websocket
+    """Class object for position storage."""
+
+    # TODO: store position and keep live returns updated via websocket
+    def __init__(self):
+        """TBC"""
+        raise NotImplementedError()
+
+    def get_positions(self):
+        """Return a list of positions for the current account."""
+        raise NotImplementedError()
+
+    def update_current_price(self, epic: str, current_price: float):
+        """Update the current value of a position based on new price data."""
+        raise NotImplementedError()
