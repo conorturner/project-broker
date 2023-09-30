87e15440208fc60ab81bb5a5045db23b95d1865d
conorturner <conorscturner@gmail.com>
2023-09-30 10:19:44 +0100

    lint message_bus.py

--- app/modules/message_bus.py
+++ app/modules/message_bus.py
@@ -1,24 +1,34 @@
-import asyncio
+"""
+Module containing the functions and classes used to pass
+live prices from broker streams to subscribers.
+"""
 
-from fastapi import WebSocket
+import asyncio
 from typing import Dict, List
+from fastapi import WebSocket
 
 
 class MessageBus:
+    """The MessageBus class is a stateful service for asynchronous messaging (many-to-many)"""
     topics: Dict[str, List[WebSocket]]
 
     def __init__(self):
+        """Initialise message bus with empty topics dictionary."""
         self.topics = {}
 
     def subscribe(self, topic: str, listener: WebSocket):
+        """Subscribes a websocket to a given topic name."""
         if topic not in self.topics:
             self.topics[topic] = []
         self.topics[topic].append(listener)
 
     def unsubscribe(self, topic: str, listener: WebSocket):
-        pass  # TODO: delete websocket from dictionary
+        """Unsubscribes a websocket to a given topic name."""
+        # TODO: delete websocket from dictionary
+        raise NotImplementedError()
 
     async def broadcast(self, topic: str, message):
+        """Broadcasts a message to all subscribers of a topic."""
         if topic not in self.topics:
             return
         # Run them all in parallel
